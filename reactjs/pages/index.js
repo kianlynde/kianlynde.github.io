eac6674f2e42aeaab9d0d7c9750a5b027880e4f1
kianlynde <43040975+kianlynde@users.noreply.github.com>
2023-11-15 14:28:06 -0700

    Update index.js

--- reactjs/pages/index.js
+++ reactjs/pages/index.js
@@ -3,8 +3,10 @@ import App from "./components/App"
 
 function H(){
     return(
-        <div>Im a silly ni</div>
-    );
+        <div>
+        <a href="https://github.com/kianlynde/kianlynde.github.io/tree/main/reactjs">Link to Github</a>
+        </div> 
+        );
 
 }
 export default H
