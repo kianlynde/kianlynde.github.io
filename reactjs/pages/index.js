cc50cab4062dd9564f75bf6d073cdb644bc52fcb
kianlynde <43040975+kianlynde@users.noreply.github.com>
2023-11-15 17:55:03 -0700

    Update index.js

--- reactjs/pages/index.js
+++ reactjs/pages/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import App from "./components/App"
+import Search from "./components/searchapi";
 
 function H(){
     return(
         <div>
-        <a href="https://github.com/kianlynde/kianlynde.github.io/tree/main/reactjs">Link to Github</a>
+            <App />
+            <div>
+                <a href="https://github.com/kianlynde/kianlynde.github.io/tree/main/reactjs">Link to Github</a>
+            </div>
+            <div>
+            </div>
         </div> 
         );
 
